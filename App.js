3308a4febd897239cd011e8f9a62df6a4c946a68
Lakhveer Bawa <bawa93lakhveer@gmail.com>
2021-04-23 21:49:17 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import {StatusBar} from 'expo-status-bar';
 import React, {useState} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Button} from 'react-native';
 
 export default function App() {
     const [name, setName] = useState('deep');
+    const clickHandler = () => {
+        setName('Chun lee')
+    }
     return (
         <View style={styles.container}>
             <View style={styles.header}><Text style={styles.boldText}>Header</Text></View>
             <Text>Open up App.js to start working on your app3!</Text>
             <View style={styles.body}><Text>My name is { name }</Text></View>
+            <View style={styles.button}>
+                <Button title="Update Me" onPress={clickHandler}/>
+            </View>
+
             <StatusBar style="auto"/>
         </View>
     );
@@ -30,5 +37,8 @@ const styles = StyleSheet.create({
     },
     body: {
         backgroundColor: 'yellow'
+    },
+    button: {
+
     }
 });
