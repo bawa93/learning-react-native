4890a8331cd630284905af04fd1b716e98267350
Lakhveer Bawa <bawa93lakhveer@gmail.com>
2021-04-23 21:41:16 +0530

    state and styles

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app3!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [name, setName] = useState('deep');
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}><Text style={styles.boldText}>Header</Text></View>
+            <Text>Open up App.js to start working on your app3!</Text>
+            <View style={styles.body}><Text>My name is { name }</Text></View>
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    header: {
+        backgroundColor: 'pink',
+        padding: 20
+    },
+    boldText: {
+        fontWeight: 'bold'
+    },
+    body: {
+        backgroundColor: 'yellow'
+    }
 });
